7c3299df4adf062ae7456bd2befc81191dced4c9
martendV <marten@de.vri.es>
2024-02-29 01:47:38 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,54 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    sitemap(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Astro PWA",
+        short_name: "Astro PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
+});
---
+++ src/env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/vanillajs" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
